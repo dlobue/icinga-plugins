c56536714a8e32bdc42430e3adf3cf086955a7fc
Dominic LoBue <dominic.lobue@gmail.com>
2011-07-12 17:27:15 -0700

    Updated process check plugin to narrow search results further by checking if the process is listening on ports supplied by the user.

--- plugins/check_clio_processes.py
+++ plugins/check_clio_processes.py
@@ -12,12 +12,17 @@ class ProcessCheck(nagiosplugin.Check):
     version = '0.1'
 
     def __init__(self, optparser, logger):
-        optparser.set_usage('usage: %prog [options] <hostname of server to check> <string to search for in process cmdline>')
+        optparser.set_usage('usage: %prog [options] <hostname of server to check> <string to search for in process cmdline> [<port process must be listening on>]')
         optparser.description = "Monitor the number of processes running with the specified name."
         optparser.version = self.version
         optparser.add_option(
             '-s', '--server', default='localhost',
             help='clio database server to query (default: %default)')
+        optparser.add_option(
+            '-p', '--port_match', default='any',
+            help=('if multiple ports are given, does a process need to contain '
+                  'all the given ports in order to match, or will any do? '
+                  '(valid values: any, all. default: %default)'))
         optparser.add_option(
             '-w', '--warning', default=None, metavar='RANGE',
             help='warning threshold (default: %default)')
@@ -29,19 +34,39 @@ class ProcessCheck(nagiosplugin.Check):
         self.warning = options.warning
         self.critical = options.critical
         self.db_server = options.server
+        self.port_match = options.port_match
+        if self.port_match not in ('any', 'all'):
+            print('Invalid match type! Valid values are "any" and "all". Got: %s' % self.port_match)
+            import sys
+            sys.exit(3)
+
         try:
-            self.server = args[0]
+            self.server = args.pop(0)
         except IndexError:
             print('What server am I supposed to check?!')
             import sys
             sys.exit(3)
 
         try:
-            self.search_string = args[1]
+            self.search_string = args.pop(0)
         except IndexError:
             print('What process am I supposed to look for?!')
             import sys
             sys.exit(3)
+
+        if args:
+            ports = set()
+            for arg in args:
+                if ',' in arg:
+                    args.extend(arg.replace(',',' ').split())
+                else:
+                    ports.add(int(arg))
+
+            self.listening_ports = ports
+
+        else:
+            self.listening_ports = None
+
 
     def obtain_data(self):
         db = pymongo.Connection(self.db_server).clio
@@ -53,15 +78,43 @@ class ProcessCheck(nagiosplugin.Check):
 
         assert (datetime.utcnow() - result['ts']).seconds < 60, "stale data! is arke running?"
 
+        def is_listening(connections, port):
+            return any((x for x in connections if x['status'] == u'LISTEN' and x['local_address'][1] == port))
+
+
+        parent_pids = []
         processes = []
         for pid,properties in result['data']['processes'].iteritems():
             if self.search_string in properties['cmdline'] and \
                properties['status'] not in ('zombie', 'dead', 'stopped', 'tracing stop'):
-                processes.append((
-                    pid,
-                    properties['name'],
-                    properties['cmdline'],
-                ))
+
+                if self.listening_ports:
+                    if getattr(__builtins__, self.port_match)((
+                        is_listening(properties['connections'], p) for p in self.listening_ports )):
+
+                        parent_pids.append(pid)
+                        processes.append((
+                            pid,
+                            properties['name'],
+                            properties['cmdline'],
+                        ))
+                else:
+                    processes.append((
+                        pid,
+                        properties['name'],
+                        properties['cmdline'],
+                    ))
+
+        if parent_pids:
+            parent_pids = map(int, parent_pids)
+            for pid,properties in result['data']['processes'].iteritems():
+                if properties['ppid'] in parent_pids:
+                    processes.append((
+                        pid,
+                        properties['name'],
+                        properties['cmdline'],
+                    ))
+
 
         self.processes = processes
         self.found_count = len(processes)
@@ -72,7 +125,7 @@ class ProcessCheck(nagiosplugin.Check):
         fields = ('pid', 'name', 'cmdline')
         processes = (pformat(zip(fields, p)) for p in self.processes)
         processes_str = '\n'.join(processes)
-        return 'Found the following processes:\n%s' % processes_str
+        return 'Found the following %i processes:\n%s' % (self.found_count, processes_str)
 
 
 main = nagiosplugin.Controller(ProcessCheck)
