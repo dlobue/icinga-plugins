65fc20816b145e3d0d4db4353c8a62bc64b0f60e
Dominic LoBue <dominic.lobue@gmail.com>
2012-08-24 21:38:45 -0700

    just work already

--- plugins/utils.py
+++ plugins/utils.py
@@ -20,26 +20,27 @@ def decode_timestamp(data):
     tzoffset = None
     if '.' in data:
         fmt += '.%f'
-        if '-' in data[data.index('T'):]:
-            tzsign = '-'
-        elif '+' in data:
-            tzsign = '+'
-        else:
-            tzsign = None
 
-        if tzsign:
-            tzidx = data.rindex(tzsign)
-            tzstr = data[tzidx:].strip(tzsign)
-            data = data[:tzidx]
-            if ':' in tzstr:
-                tzhours,tzminutes = map(int, (tzsign+_ for _ in tzstr.split(':')))
-            elif len(tzstr) == 4:
-                tzhours = int(tzsign+tzstr[:2])
-                tzminutes = int(tzsign+tzstr[2:])
-            else:
-                logger.warn('unrecognized timezone format. assuming tz to be UTC')
-                tzhours,tzminutes = 0,0
-            tzoffset = timedelta(hours=tzhours, minutes=tzminutes)
+    if '-' in data[data.index('T'):]:
+        tzsign = '-'
+    elif '+' in data:
+        tzsign = '+'
+    else:
+        tzsign = None
+
+    if tzsign:
+        tzidx = data.rindex(tzsign)
+        tzstr = data[tzidx:].strip(tzsign)
+        data = data[:tzidx]
+        if ':' in tzstr:
+            tzhours,tzminutes = map(int, (tzsign+_ for _ in tzstr.split(':')))
+        elif len(tzstr) == 4:
+            tzhours = int(tzsign+tzstr[:2])
+            tzminutes = int(tzsign+tzstr[2:])
+        else:
+            logger.warn('unrecognized timezone format. assuming tz to be UTC')
+            tzhours,tzminutes = 0,0
+        tzoffset = timedelta(hours=tzhours, minutes=tzminutes)
 
 
 
